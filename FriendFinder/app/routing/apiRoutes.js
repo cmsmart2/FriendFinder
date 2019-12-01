235672f7c4b09abbc4a080bdecdc3039a101aced
Cera Smart <cera_smart@yahoo.com>
2019-12-01 13:33:14 -0600

    fixed matching function but pictures still not showing

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -5,31 +5,31 @@ module.exports = function(app){
         res.json(friends)
     })
 
-app.post("/api/friends", function(req, res){
-    const userAnswers = req.body;
-    const userScore = userAnswers.score;
-    // console.log("answers" + userAnswers)
-    // console.log("score: " + userScore)
-    let match = {
-        name: "",
-        photo: "",
-        difference: 100
-    };
+    app.post("/api/friends", function(req, res){
+        const userAnswers = req.body;
+        const userScore = userAnswers.score;
+        // console.log("answers" + userAnswers)
+        // console.log("score: " + userScore)
+        let match = {
+            name: "",
+            photo: "",
+            difference: 50
+        };
 
-    let totalDifference = 0;
-
-for (var a =0; a<friends.length; a++){
-    totalDifference =0;
-    for (var i = 0; i< friends[a].scores[i]; i++){
-        totalDifference += Math.abs(parseInt(userScore[i])- parseInt(friends[a].scores[i]))
-        if (totalDifference <= match.difference){
-            match.name = friends[a].name
-            match.photo = friends[a]. photo
-            match.difference = totalDifference
+        let totalDifference = 0;
+    
+    for (var a =0; a<friends.length; a++){
+        totalDifference =0;
+        for (var i = 0; i< friends[a].scores[i]; i++){
+            totalDifference += Math.abs(parseInt(userScore[i])- parseInt(friends[a].scores[i]))
+            if (totalDifference <= match.difference){
+                match.name = friends[a].name
+                match.photo = friends[a]. photo
+                match.difference = totalDifference
+                    }
                 }
             }
-        }
         console.log(match)
-    res.json(match);
+        res.json(match);
     });
 };

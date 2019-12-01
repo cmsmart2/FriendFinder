fcd20153d0468745d6866ecf00be6ad2ccd8b4ca
Cera Smart <cera_smart@yahoo.com>
2019-12-01 11:12:41 -0600

    added logic to find best match

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -4,12 +4,31 @@ module.exports = function(app){
     app.get("/api/friends", function(req,res){
         res.json(friends)
     })
-}
+
 app.post("/api/friends", function(req, res){
+    const userAnswers = req.body;
+    const userScore = userAnswers.score;
+    // console.log("answers" + userAnswers)
+    // console.log("score: " + userScore)
     let match = {
         name: "",
         photo: "",
         difference: 100
     };
-    console.log(req.body)
-})
+
+    let totalDifference = 0;
+
+for (var a =0; a<friends.length; a++){
+    for (var i = 0; i< friends[a].scores[i]; i++){
+        totalDifference += Math.abs(parseInt(userScore[i])- parseInt(friends[a].scores[i]))
+        if (totalDifference <= match.difference){
+            match.name = friends[a].name
+            match.photo = friends[a]. photo
+            match.difference = totalDifference
+                }
+            }
+        }
+        console.log(match)
+    res.json(match);
+    });
+};
